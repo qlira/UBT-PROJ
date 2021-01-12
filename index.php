e758ca5f3d285be785341f7cc53004ca8920226b
Çlirim Krasniqi <qliraa19@gmail.com>
2021-01-12 21:30:26 +0100

    Update

--- index.php
+++ index.php
@@ -7,6 +7,12 @@
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.gstatic.com/%22%3E
+  <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@500&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.gstatic.com/%22%3E
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+
 
   <!-- FontAwesome Import -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
